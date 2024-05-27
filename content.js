b5243d588a30c4ff51405747d39e8333f63a0e7e
JdM <julien@rmbi.ch>
2024-05-27 20:20:24 +0200

    first commit

---
+++ content.js
@@ -0,0 +1,11 @@
+document.body.innerHTML = /*html*/`
+<h1>HAHAHAHAHAHA</h1>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2qBlE2-WL60?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<br>
+<img src="https://media1.tenor.com/m/H6KZbyf7dzYAAAAd/laughing-hysterically-laughing.gif"/>
+<img src="https://64.media.tumblr.com/d94604584cf09e22a6bca32cb3ef1a05/tumblr_n2408e60Y21qlvie8o4_250.gifv"/>
+<img src="https://media1.tenor.com/m/bneji5SU-w4AAAAC/jim-carrey-haha.gif"/>
+<h2>LOOOLLL</h2>
+`;
+
+let head = document.head || document.getElementsByTagName('head')[0]
