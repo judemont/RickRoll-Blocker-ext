c0cddcf2f4dccb90b182a15ff4b7901f351e9edd
JdM <julien@rmbi.ch>
2024-05-27 20:50:18 +0200

    Update: Refactor content.js and manifest.json files

--- content.js
+++ content.js
@@ -1,3 +1,4 @@
+if(window.location.host != "github.com"){
 document.body.innerHTML = /*html*/`
 <h1>HAHAHAHAHAHA</h1>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/2qBlE2-WL60?autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
@@ -7,5 +8,4 @@ document.body.innerHTML = /*html*/`
 <img src="https://media1.tenor.com/m/bneji5SU-w4AAAAC/jim-carrey-haha.gif"/>
 <h2>LOOOLLL</h2>
 `;
-
-let head = document.head || document.getElementsByTagName('head')[0]
+}
